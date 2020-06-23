53e4eb02487399bcca03b8fa7c311425b1432991
jade.h <jade.h@kakaocorp.com>
2020-06-23 16:47:56 +0900

    프로젝트 생성

---
+++ src/main/java/kr/ac/jejunu/trade/TradeApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeApplication.class, args);
+    }
+
+}
