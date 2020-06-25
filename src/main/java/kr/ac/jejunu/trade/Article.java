ae99f91ac6bf1216ddf1a6cf043da3c9b161ca8c
jade.h <jade.h@kakaocorp.com>
2020-06-25 18:06:01 +0900

    게시글 모델 구현

--- src/main/java/kr/ac/jejunu/trade/Article.java
+++ src/main/java/kr/ac/jejunu/trade/Article.java
@@ -5,12 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @ManyToOne
     private User writer;
     private String title;
     private String content;
---
+++ src/main/java/kr/ac/jejunu/trade/ArticleRepository.java
@@ -0,0 +1,12 @@
+package kr.ac.jejunu.trade;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+}
